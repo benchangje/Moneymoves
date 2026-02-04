3e1ea46cee7339115ffbd87b11e34c1c978643bf
Benjamin Chang <benchang2611@gmail.com>
2026-02-04 13:19:07 +0800

    Update IndexPage (saved)

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -25,7 +25,7 @@ export const IndexPage: FC = () => {
         </Section>
         <Section
           header="Application Launch Data"
-          footer="These pages help developer to learn more about current launch information hi"
+          footer="These pages help developer to learn more about current launch information"
         >
           <Link to="/init-data">
             <Cell subtitle="User data, chat information, technical data">Init Data</Cell>
