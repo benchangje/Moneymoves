09604f806cdbdd4f3301de0e5b41beed175fbefe
Benjamin Chang <benchang2611@gmail.com>
2026-02-07 23:54:39 +0800

    Added new features to test Vercel preview

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ export default function Navbar() {
     return <nav className="fixed top-0 w-full bg-slate-950/20 backdrop-blur-sm z-50 transition-all duration-300">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"> 
             <div className="flex items-center justify-between h-14 sm:h-16 md"> 
-                
+                hi there, this is a rental marketplace! 
+                <div>hello!</div>
             </div>
 
         </div>
