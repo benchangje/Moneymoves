76e7b0b078b952bc4723ad0ce66b64d1eb3b1779
Benjamin Chang <benchang2611@gmail.com>
2026-02-08 14:57:55 +0800

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 export default function Navbar() {
     return <nav className="fixed top-0 w-full bg-slate-950/20 backdrop-blur-sm z-50 transition-all duration-300">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"> 
-            <div className="flex items-center justify-between h-14 sm:h-16 md"> 
-                <div> 
-                    <img src="/logo.png" alt="logo" className="h-6 w-6 sm:h-8 sm:w-8"/>
+           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"> 
+                <div className="flex items-center justify-between h-14 sm:h-16 md"> 
+                    <div className="flex items-center space-x-2 group cursor-pointer">
+                        <div> 
+                            <img src="/logo.png" alt="logo" className="h-6 w-6 sm:h-8 sm:w-8"/>
+                        </div>
+                        <span className="text-lg sm:text-xl md:text-2xl font-bold">
+                            <span className="text-white font-bold">Money</span>
+                            <span className="text-blue-400 font-bold">Moves</span>
+                        </span>
+                    </div>
                 </div>
-                <span>
-                    <span className="text-white font-bold">Money</span>
-                    <span className="text-blue-400 font-bold">Moves</span>
-                </span>
-            </div>
-        </div> 
-    </nav>
+            </div> 
+            </nav>
 }
