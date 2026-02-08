b13852d5401077d2d488d73d4851aac40e6dc3d1
Benjamin Chang <benchang2611@gmail.com>
2026-02-08 14:51:50 +0800

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,14 @@ export default function Navbar() {
     return <nav className="fixed top-0 w-full bg-slate-950/20 backdrop-blur-sm z-50 transition-all duration-300">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"> 
             <div className="flex items-center justify-between h-14 sm:h-16 md"> 
-                hi there, this is a rental marketplace! 
-                <div>hello!</div>
-                <div>hi there!</div>
+                <div> 
+                    <img src="/logo.png" alt="logo" className="h-6 w-6 sm:h-8 sm:w-8"/>
+                </div>
+                <span>
+                    <span className="text-white font-bold">Money</span>
+                    <span className="text-blue-400 font-bold">Moves</span>
+                </span>
             </div>
-
-        </div>
-        
+        </div> 
     </nav>
 }
